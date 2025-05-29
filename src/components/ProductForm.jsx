06101b073adf263651d683823f3b21aad8ca34e9
Marcelo <marcelopacobello@gmail.com>
2025-05-29 19:59:41 -0300

    fix: evita sobreposição do label nos campos do formulário ao editar produto (shrink: true)

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -24,22 +24,22 @@ export default function ProductForm({ onSave, editando, onCancel }) {
       <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
-            <TextField label="Nome" fullWidth {...register('nome', { required: true })} error={!!errors.nome} helperText={errors.nome && 'Campo obrigatório'} InputLabelProps={{ style: { fontWeight: 'bold' } }} inputProps={{ style: { fontWeight: 'bold' } }} />
+            <TextField label="Nome" fullWidth {...register('nome', { required: true })} error={!!errors.nome} helperText={errors.nome && 'Campo obrigatório'} InputLabelProps={{ style: { fontWeight: 'bold' }, shrink: true }} inputProps={{ style: { fontWeight: 'bold' } }} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField label="Marca" fullWidth {...register('marca', { required: true })} error={!!errors.marca} helperText={errors.marca && 'Campo obrigatório'} InputLabelProps={{ style: { fontWeight: 'bold' } }} inputProps={{ style: { fontWeight: 'bold' } }} />
+            <TextField label="Marca" fullWidth {...register('marca', { required: true })} error={!!errors.marca} helperText={errors.marca && 'Campo obrigatório'} InputLabelProps={{ style: { fontWeight: 'bold' }, shrink: true }} inputProps={{ style: { fontWeight: 'bold' } }} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField label="Tipo" fullWidth {...register('tipo', { required: true })} error={!!errors.tipo} helperText={errors.tipo && 'Campo obrigatório'} InputLabelProps={{ style: { fontWeight: 'bold' } }} inputProps={{ style: { fontWeight: 'bold' } }} />
+            <TextField label="Tipo" fullWidth {...register('tipo', { required: true })} error={!!errors.tipo} helperText={errors.tipo && 'Campo obrigatório'} InputLabelProps={{ style: { fontWeight: 'bold' }, shrink: true }} inputProps={{ style: { fontWeight: 'bold' } }} />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField label="Quantidade" type="number" fullWidth {...register('quantidade', { required: true, min: 0 })} error={!!errors.quantidade} helperText={errors.quantidade && 'Campo obrigatório (mínimo 0)'} inputProps={{ min: 0, style: { fontWeight: 'bold' } }} InputLabelProps={{ style: { fontWeight: 'bold' } }} />
+            <TextField label="Quantidade" type="number" fullWidth {...register('quantidade', { required: true, min: 0 })} error={!!errors.quantidade} helperText={errors.quantidade && 'Campo obrigatório (mínimo 0)'} inputProps={{ min: 0, style: { fontWeight: 'bold' } }} InputLabelProps={{ style: { fontWeight: 'bold' }, shrink: true }} />
           </Grid>
           <Grid item xs={12}>
-            <TextField label="Descrição" fullWidth multiline rows={2} {...register('descricao')} InputLabelProps={{ style: { fontWeight: 'bold' } }} inputProps={{ style: { fontWeight: 'bold' } }} />
+            <TextField label="Descrição" fullWidth multiline rows={2} {...register('descricao')} InputLabelProps={{ style: { fontWeight: 'bold' }, shrink: true }} inputProps={{ style: { fontWeight: 'bold' } }} />
           </Grid>
           <Grid item xs={12}>
-            <TextField label="URL da Imagem" fullWidth {...register('imagem')} InputLabelProps={{ style: { fontWeight: 'bold' } }} inputProps={{ style: { fontWeight: 'bold' } }} />
+            <TextField label="URL da Imagem" fullWidth {...register('imagem')} InputLabelProps={{ style: { fontWeight: 'bold' }, shrink: true }} inputProps={{ style: { fontWeight: 'bold' } }} />
           </Grid>
           <Grid item xs={12}>
             <Box display="flex" gap={2}>
